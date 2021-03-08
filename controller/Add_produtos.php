c7de8b280e516ae62d05fd0c63b33102f2bb3323
Daniel Ferraz <Daniel0ferraz@outlook.com>
2021-03-07 22:13:16 -0300

    refatorando

--- controller/Add_produtos.php
+++ controller/Add_produtos.php
@@ -1,2 +1,27 @@
 <?php
+        
+        if (isset($_POST['CODIGO']) && empty($_POST['CODIGO']) == false) {
+
+            $CODIGO = addslashes($_POST['CODIGO']);
+            $DESCRICAO = addslashes($_POST['DESCRICAO']);
+            $VALOR = addslashes($_POST['VALOR']);
+            $OBS = addslashes($_POST['OBS']);
+            $DATA = addslashes($_POST['DATA']);
+            $QUANTIDADE = addslashes($_POST['QUANTIDADE']);
+            $MOTIVO = addslashes($_POST['MOTIVO']);
+
+            $sql = "INSERT INTO entrada SET CODIGO = '$CODIGO', DESCRICAO = '$DESCRICAO', VALOR = '$VALOR', OBS = '$OBS', DATA = NOW(), QUANTIDADE = '$QUANTIDADE', MOTIVO ='$MOTIVO'";
+            $pdo->query($sql);
+
+            header('Location: index.php');
+
+            if ($MOTIVO == 'EMPRESTIMO') {
+              $sql = $pdo->prepare("UPDATE entrada SET QUANTIDADE = QUANTIDADE - :VAL_TOTAL WHERE id = :id "); 
+              $sql->bindValue(":MOTIVO", $MOTIVO);
+              $sql->bindValue(":CODIGO", $CODIGO);
+              $sql->execute();
+          } 
+
+        } 
+        ?>
 
--- entrada.php
+++ entrada.php
@@ -42,33 +42,6 @@ if (isset($_SESSION['estoque'])) {
   <link rel="stylesheet" href="css/form.css">
   <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Ubuntu:wght@300;700&display=swap" rel="stylesheet">
 </head>
-<?php
-           
-            if (isset($_POST['CODIGO']) && empty($_POST['CODIGO']) == false) {
-
-                $CODIGO = addslashes($_POST['CODIGO']);
-                $DESCRICAO = addslashes($_POST['DESCRICAO']);
-                $VALOR = addslashes($_POST['VALOR']);
-                $OBS = addslashes($_POST['OBS']);
-                $DATA = addslashes($_POST['DATA']);
-                $QUANTIDADE = addslashes($_POST['QUANTIDADE']);
-                $MOTIVO = addslashes($_POST['MOTIVO']);
-
-                $sql = "INSERT INTO entrada SET CODIGO = '$CODIGO', DESCRICAO = '$DESCRICAO', VALOR = '$VALOR', OBS = '$OBS', DATA = NOW(), QUANTIDADE = '$QUANTIDADE', MOTIVO ='$MOTIVO'";
-                $pdo->query($sql);
-
-                header('Location: index.php');
-
-                if ($MOTIVO == 'EMPRESTIMO') {
-                  $sql = $pdo->prepare("UPDATE entrada SET QUANTIDADE = QUANTIDADE - :VAL_TOTAL WHERE id = :id "); 
-                  $sql->bindValue(":MOTIVO", $MOTIVO);
-                  $sql->bindValue(":CODIGO", $CODIGO);
-                  $sql->execute();
-              } 
-
-            } 
-            ?>
-
 <body>
 
   <div class="container">
